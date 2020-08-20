4ceaef3d1f14693f66d7d5c59f0f2b657bd1684a
Hugo Herrador Carrasco <h.herrador@nevintec.local>
2020-08-20 11:35:41 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DrawComponent } from './widgets/draw/draw.component';
+import { MeasureComponent } from './widgets/measure/measure.component';
+import { MarkerComponent } from './widgets/marker/marker.component';
+import { MapComponent } from './components/map/map.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { LeftbarComponent } from './components/leftbar/leftbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrawComponent,
+    MeasureComponent,
+    MarkerComponent,
+    MapComponent,
+    TopbarComponent,
+    LeftbarComponent
   ],
   imports: [
     BrowserModule
