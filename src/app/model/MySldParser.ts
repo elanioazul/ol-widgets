d5e0ff5a3d9a096ebd08a5c279ec857b50ea6cf6
Hugo Herrador Carrasco <h.herrador@nevintec.local>
2020-09-11 14:45:52 +0200

    Accepts now base64 icons

--- src/app/model/MySldParser.ts
+++ src/app/model/MySldParser.ts
@@ -10,16 +10,44 @@ import {
 export class MySldParser extends SLDParser {
 
     getIconSymbolizerFromSldSymbolizer(sldSymbolizer): any {
-        alert("HOLA");
-
-        const inlineContent = _get(sldSymbolizer, 'Graphic[0].ExternalGraphic[0].InlineContent[0]');
-        const stringFromBase64 = btoa(inlineContent.$['encoding']);
-        //const stringFromBase64_2 = XMLDocument.getElementByTagName("inlineContent")[0].childNodes[0].nodeValue;
-        const iconSymbolizer: IconSymbolizer = <IconSymbolizer> {
-            kind: 'Icon',
-            image: stringFromBase64
-        };
-
+        /*       debugger
+               let onlineResource = _get(sldSymbolizer, 'Graphic[0].ExternalGraphic[0].OnlineResource[0]');
+               //let inlineContent = _get(sldSymbolizer, 'Graphic[0].ExternalGraphic[0].InlineContent[0].childNodes[0].nodeValue');
+               let inlineContent = _get(sldSymbolizer, 'Graphic[0].ExternalGraphic[0].InlineContent[0]');
+               let imageToLoad = function() {
+                   if (onlineResource = undefined) {
+                       //let block = inlineContent._.replace(/(\r\n|\n|\r)/gm, '');
+                       let block = inlineContent.replace(/↵/ig, '');
+                       //let block = inlineContent.$.nodeValue;
+                       let format = block[0].split(":")[1];
+                       let imagedata = block[1].split(",")[1];
+                       return [format, imagedata]
+                       //return imagedata
+                   } else {
+                       let onlineResourceURI = onlineResource.$['xlink:href']
+                       return onlineResourceURI
+                   }
+               }
+               const iconSymbolizer: IconSymbolizer = <IconSymbolizer> {
+                   kind: 'Icon',
+                   image: imageToLoad()
+               };
+       */
+        const externalGraphic = _get(sldSymbolizer, 'Graphic[0].ExternalGraphic[0]');
+        const onlineResource = externalGraphic.OnlineResource ? externalGraphic.OnlineResource[0] : null;// _get(sldSymbolizer, 'Graphic[0].ExternalGraphic[0].OnlineResource[0]');
+        var iconSymbolizer: IconSymbolizer;
+        if (onlineResource) {
+            iconSymbolizer = <IconSymbolizer>{
+                kind: 'Icon',
+                image: onlineResource.$['xlink:href']
+            };
+        } else if (externalGraphic.InlineContent) {
+            let inlineContent = externalGraphic.InlineContent[0]._.trim();
+            iconSymbolizer = <IconSymbolizer>{
+                kind: 'Icon',
+                image: inlineContent
+            };
+        }
 
         var opacity = _get(sldSymbolizer, 'Graphic[0].Opacity[0]');
         var size = _get(sldSymbolizer, 'Graphic[0].Size[0]');
